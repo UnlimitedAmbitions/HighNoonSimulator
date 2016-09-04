5e7a0e9e79c95d2887b9d317320c9cc8c4e401d4
Naly-chan <gracefulshawol@gmail.com>
2016-09-04 18:07:06 -0400

    added animations to transition

---
+++ Assets/Scripts/MainMenuCameraTurn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCameraTurn : MonoBehaviour {
+	public Animator camAnim;
+	
+	public void ToStats(){
+		camAnim.SetTrigger("MainToStats");
+	}
+
+	public void ToMainMenu(){
+		camAnim.SetTrigger("StatsToMain");
+	}
+}
