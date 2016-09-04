bbd198284492ddc900b66cf542c4b05219e61cff
David Tran <dt_dbqt@hotmail.com>
2016-09-04 19:05:39 -0400

    connected main menu to game scene

--- Assets/Scripts/MainMenuCameraTurn.cs
+++ Assets/Scripts/MainMenuCameraTurn.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenuCameraTurn : MonoBehaviour {
 	public Animator camAnim;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("game");
+    }
 	
 	public void ToStats(){
 		camAnim.SetTrigger("MainToStats");
