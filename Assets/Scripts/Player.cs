f3586de75f5778e0e8cee0fb1e79681685570eae
Naly-chan <gracefulshawol@gmail.com>
2016-09-04 17:00:43 -0400

    added main menu GUI + player code
    - added main menu scene : statistics canvas & main menu canvas
    - completed Player.cs
    - changed aspect ratio to Android

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,28 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	private int hp;
+	public int decreaseHPThreshold;
+	public int[] damageShots;
 
 	// Use this for initialization
 	void Start () {
-	
+		resetHP();
 	}
+	/*
+        scalingRate = allScalingRates[Mathf.FloorToInt(Random.Range(0, allScalingRates.Length))];
+        */
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		// Randomly deduct hp
+		if(Mathf.FloorToInt(Random.Range(0, 100)) > decreaseHPThreshold){
+			hp -= damageShots[Mathf.FloorToInt(Random.Range(0, damageShots.Length))];
+
+			// If dead, alert GameManager
+			if(hp <= 0) GameManager.instance.ImDead();
+		}
 	}
+
+	public void resetHP(){ hp = 200; }
 }
