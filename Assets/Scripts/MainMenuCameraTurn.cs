8bf77f38adbf4b33c60c162afe1a33da1e4779fd
Naly-chan <gracefulshawol@gmail.com>
2016-09-04 19:24:17 -0400

    added High Noon sound and gunshots on button

--- Assets/Scripts/MainMenuCameraTurn.cs
+++ Assets/Scripts/MainMenuCameraTurn.cs
@@ -4,17 +4,21 @@ using System.Collections;
 
 public class MainMenuCameraTurn : MonoBehaviour {
 	public Animator camAnim;
+	public AudioSource gunshot;
 
     public void StartGame()
     {
         SceneManager.LoadScene("game");
+        gunshot.Play();
     }
 	
 	public void ToStats(){
 		camAnim.SetTrigger("MainToStats");
+        gunshot.Play();
 	}
 
 	public void ToMainMenu(){
 		camAnim.SetTrigger("StatsToMain");
+        gunshot.Play();
 	}
 }
