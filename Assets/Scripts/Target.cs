eb6b750545c227c92e2ed0ee35ea9f9f494a6af7
David Tran <dt_dbqt@hotmail.com>
2016-09-04 15:29:25 -0400

    init

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    public GameObject target;
+    public Image circle;
+
+    public float scalingRate;
+    public float[] allScalingRates;
+    public float startingScale;
+
+    private bool wouldDie;
+    private float scale;
+
+	// Use this for initialization
+	void Start () {
+        scale = startingScale;
+        wouldDie = false;
+        scalingRate = allScalingRates[Mathf.FloorToInt(Random.Range(0, allScalingRates.Length))];
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(wouldDie) return;
+
+        scale -= scalingRate;
+        if(scale < 0f) scale = 0f;
+
+        circle.transform.localScale = new Vector3(scale, scale, scale);
+
+        if (scale <= 0f) {
+            wouldDie = true;
+            setSkull();
+        }
+	
+	}
+
+    private void setSkull() {
+        // set skull
+    }
+}
