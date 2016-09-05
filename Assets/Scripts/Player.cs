8e485996660c48985287383fe19c41ba226563d2
David Tran <David Tran>
2016-09-05 15:02:16 -0400

    added arm and dmg overlay

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ public class Player : MonoBehaviour {
 	public int hp;
 	public int decreaseHPThreshold;
 	public int[] damageShots;
+	public GameObject dmgOverlay;
+	public float dmgOverlayDuration;
 
 	// Use this for initialization
 	void Start () {
 		resetHP();
+		resetDmgOverlay();
 	}
 	/*
         scalingRate = allScalingRates[Mathf.FloorToInt(Random.Range(0, allScalingRates.Length))];
@@ -19,11 +22,15 @@ public class Player : MonoBehaviour {
 		// Randomly deduct hp
 		if(GameManager.instance.gameStarted && Mathf.FloorToInt(Random.Range(0, 100)) > decreaseHPThreshold){
 			hp -= damageShots[Mathf.FloorToInt(Random.Range(0, damageShots.Length))];
-
+			dmgOverlay.SetActive(true);
+			Invoke("resetDmgOverlay", dmgOverlayDuration);
 			// If dead, alert GameManager
 			if(hp <= 0) GameManager.instance.ImDead();
 		}
 	}
 
 	public void resetHP(){ hp = 200; }
+
+	public void resetDmgOverlay() { dmgOverlay.SetActive(false); }
+
 }
